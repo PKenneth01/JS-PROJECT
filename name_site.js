b55396373cac41c6b35b9808cc833971d9e0cc10
PKenneth01 <kennethkabutu@rocketmail.com>
2022-10-03 14:45:27 +0300

    js indentation adjusted

--- name_site.js
+++ name_site.js
@@ -40,40 +40,35 @@ function get_akan_name() {
   let month = document.getElementById("mob").value;
   let year = document.getElementById("yob").value;
 
-/**check validity */
+  /**check validity */
   if (!gender) {
-    gender = prompt("Please add a valid gender")
+     gender = prompt("Please add a valid gender")
   }
   if (day <= 0) {
-    day = prompt("day is less than 1. Please add a valid day of birth")
-  }
-  else if (day > 31) {
-    day = prompt("day is greater than 31. Please add a valid day of birth")
-  }
-  else if (month <= 0) {
-    month = prompt("month is less than 1. Please add a valid month of birth")
-  }
-  else if (month > 12) {
-    month = prompt("month is greater than 12. Please add a valid month of birth")
-  }
-  else {
+     day = prompt("day is less than 1. Please add a valid day of birth")
+  } else if (day > 31) {
+     day = prompt("day is greater than 31. Please add a valid day of birth")
+  } else if (month <= 0) {
+     month = prompt("month is less than 1. Please add a valid month of birth")
+  } else if (month > 12) {
+     month = prompt("month is greater than 12. Please add a valid month of birth")
+  } else {
 
 
-    /** parameters */
-    const century = year.substring(0, 2);
-    const twoLetterYear = year.substring(2, 4);
-    // call day_of_week_formular function;
-    const returned_day_of_week = day_of_week_formular(century, twoLetterYear, month, day);
-    // console.log(returned_day_of_week)
+     /** parameters */
+     const century = year.substring(0, 2);
+     const twoLetterYear = year.substring(2, 4);
+     // call day_of_week_formular function;
+     const returned_day_of_week = day_of_week_formular(century, twoLetterYear, month, day);
+     // console.log(returned_day_of_week)
 
-    // print the day the person was born
-    let message = "";
-    if (gender == "male") {
-      message = "You were born on " + days_of_the_week[returned_day_of_week] + ". So, your Akan Name is " + male_names[returned_day_of_week]
-    }
-    else {
-      message = "You were born on " + days_of_the_week[returned_day_of_week] + ". So, your Akan Name is " + female_names[returned_day_of_week]
-    }
-    alert(message);
+     // print the day the person was born
+     let message = "";
+     if (gender == "male") {
+        message = "You were born on " + days_of_the_week[returned_day_of_week] + ". So, your Akan Name is " + male_names[returned_day_of_week]
+     } else {
+        message = "You were born on " + days_of_the_week[returned_day_of_week] + ". So, your Akan Name is " + female_names[returned_day_of_week]
+     }
+     alert(message);
   }
 }
